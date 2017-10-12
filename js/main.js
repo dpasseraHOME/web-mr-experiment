a903b86410702ed97d9223c2da71279971d70ee8
Dante Passera <dpassera@centerline.net>
2017-10-12 12:24:20 -0400

    getUserMedia test

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 function init() {
 	console.log("# init");
+
+	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+	var videoElement = document.getElementById("camera-stream");
+
+	navigator.getUserMedia({video:true}, function (stream){
+            if (window.URL) {
+                    videoElement.src = window.URL.createObjectURL(stream);
+            } else if (videoElement.mozSrcObject !== undefined) {
+                    videoElement.mozSrcObject = stream;
+            } else {
+                    videoElement.src = stream;
+            }
+        },
+        function(error){
+        }
+    );
 }
