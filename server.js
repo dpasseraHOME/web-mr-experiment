0ec0bf378089aaf727f28ec24b6442a100ab69de
Dante Passera <dpassera@centerline.net>
2017-10-12 15:09:26 -0400

    full viewport video

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
-/*var server = http.createServer(function(req, res) {
-res.writeHead(200);
-res.end('Hi everybody!');
-});
-server.listen(8080);*/
-
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
